49a3066327fbc99cc9dcdcb42735281018469d38
Fırat Can Taş <firatcantass@gmail.com>
2023-07-17 11:54:07 +0300

    🌱 code standards improved

--- src/ChatwootChat.js
+++ src/ChatwootChat.js
@@ -12,11 +12,9 @@ const ChatwootChat = () => {
   const [status, setStatus] = useState('Send Message:') // Sohbet durumunu temsil eden state
   const [socket, setSocket] = useState(null) // Socket.io bağlantısını temsil eden state
   const inboxIdentifier = 'VHSzin56Hodyxx9B8VuDZiu2' // Chatwoot sunucusundaki gelen kutusu kimliği
-  let contactIdentifier = '' // Kullanıcının kimliği
-  let contactPubsubToken = '' // Kullanıcının pubsub token'ı
-  let contactConversation = '' // Kullanıcının sohbet kimliği
-
-
+  const [contactIdentifier, setContactIdentifier] = useState('') // Kullanıcının kimliği
+  const [contactPubsubToken, setContactPubsubToken] = useState('') // Kullanıcının pubsub token'ı
+  const [contactConversation, setContactConversation] = useState('') // Kullanıcının sohbet kimliği
 
   useEffect(() => {
 
@@ -25,9 +23,9 @@ const ChatwootChat = () => {
       try {
         const response = await axios.post(`https://app.chatwoot.com/public/api/v1/inboxes/${inboxIdentifier}/contacts`) //Geriye gerekli bilgileri alabilmek için post atıyoruz
 
-        contactPubsubToken = response.data.pubsub_token //Pubsub token, publish-subscribe modeline dayalı iletişimi sağlamak için kullanılır. Bu token, kullanıcının belirli bir sohbet kanalına abone olmasını ve o kanal üzerinden mesaj gönderip almasını sağlar.
+        setContactPubsubToken(response.data.pubsub_token) //Pubsub token, publish-subscribe modeline dayalı iletişimi sağlamak için kullanılır. Bu token, kullanıcının belirli bir sohbet kanalına abone olmasını ve o kanal üzerinden mesaj gönderip almasını sağlar.
 
-        contactIdentifier = response.data.source_id //Kaynak kimliği ise kullanıcının Chatwoot sunucusunda benzersiz bir tanımlayıcıdır. Bu kimlik, kullanıcının belirli bir sohbeti temsil eder ve sunucu üzerindeki işlemleri bu kimlik aracılığıyla yapabiliriz.
+        setContactIdentifier(response.data.source_id) //Kaynak kimliği ise kullanıcının Chatwoot sunucusunda benzersiz bir tanımlayıcıdır. Bu kimlik, kullanıcının belirli bir sohbeti temsil eder ve sunucu üzerindeki işlemleri bu kimlik aracılığıyla yapabiliriz.
 
       } catch (error) {
         console.log('Contact setup error:', error)
@@ -41,20 +39,20 @@ const ChatwootChat = () => {
       try {
         const response = await axios.post(`https://app.chatwoot.com/public/api/v1/inboxes/${inboxIdentifier}/contacts/${contactIdentifier}/conversations`) //Geriye gerekli bilgileri alabilmek için post atıyoruz
 
-        contactConversation = response.data.id //Sohbet kimliği (contactConversation), Chatwoot platformunda belirli bir müşteri ile yapılan sohbetin benzersiz kimliğidir.
+        setContactConversation(response.data.id) //Sohbet kimliği (contactConversation), Chatwoot platformunda belirli bir müşteri ile yapılan sohbetin benzersiz kimliğidir.
 
 
       } catch (error) {
         console.log('Conversation setup error:', error)
       }
 
-    };
+    }
 
-
+    
 
     const connectToChatwoot = () => {
 
-      const socket = io('http://localhost:3000') // Socket.io sunucusuna bağlanma
+      const socket = io('https://app.chatwoot.com') // Socket.io sunucusuna bağlanma
       setSocket(socket)
 
       socket.on('connect', () => { // Socket.io bağlantısı başarılı olduğunda çalışacak kodlar
@@ -69,23 +67,27 @@ const ChatwootChat = () => {
           identifier: JSON.stringify(subscriptionParams),
         }
 
-        socket.send(JSON.stringify(subscription)); // WebSocket üzerinden abonelik mesajını gönderme
+        socket.send(JSON.stringify(subscription)) // WebSocket üzerinden abonelik mesajını gönderme
+        
         setStatus('Send Message:') // Durumu güncelleme
-
+        
       })
 
 
-      socket.on('message', (message) => { // Mesaj alma olayını dinleme
+      socket.on('message', (message) => {
 
         const json = JSON.parse(message)
-        
+
         if (json.type === 'welcome' || json.type === 'ping' || json.type === 'confirm_subscription') {
-          return;
+
+          return
         } 
-        
+
         else if (json.message && json.message.event === 'message.created') {
-          const { name, content } = json.message.data.sender;
-          addMessage(name, content) // Gelen mesajı ekleme
+
+          const { name, content } = json.message.data.sender
+          addMessage(name, content)
+
         } 
 
         else {
@@ -94,7 +96,7 @@ const ChatwootChat = () => {
 
       })
 
-      socket.on('error', (error) => { // Hata durumunda çalışacak kodlar
+      socket.on('error', (error) => {
         console.log('WebSocket connection error:', error)
       })
     }
@@ -107,47 +109,52 @@ const ChatwootChat = () => {
       })
 
 
-    return () => { // Bileşenin temizlendiği durumda socket bağlantısını kapatma
+    return () => {
+
       if (socket) {
         socket.close()
       }
     }
-    
-  }, [])
+
+  }, [contactIdentifier, contactPubsubToken, inboxIdentifier])
 
 
 
   const addMessage = (author, message) => {
-    setContent((prevContent) => [...prevContent, { author, message }]); // Yeni mesajı içeriğe ekleme
+
+    setContent((prevContent) => [...prevContent, { author, message }])
   }
 
 
   const sendMessage = () => {
 
     if (!inputValue) {
-      return;
+      return
     }
 
     const message = { content: inputValue }
 
-    axios.post(`https://app.chatwoot.com/public/api/v1/inboxes/${inboxIdentifier}/contacts/${contactIdentifier}/conversations/${contactConversation}/messages`, message)
+    axios.post(`https://app.chatwoot.com/public/api/v1/inboxes/${inboxIdentifier}/contacts/${contactIdentifier}/conversations/${contactConversation}/messages`,message)
+
       .then(() => {
-        addMessage('me', inputValue); // Gönderilen mesajı ekleme
+        addMessage('me', inputValue)
         setInputValue('')
       })
+
       .catch((error) => {
+        
         console.log('Message sending error:', error)
-        console.log(`Veriler burada ${inboxIdentifier} AND ${contactIdentifier} AND ${contactConversation}`);
       })
-      
+
   }
 
 
 
   const handleInputChange = (event) => {
-    setInputValue(event.target.value); // Metin girişi değişikliğini takip etme
+
+    setInputValue(event.target.value)
   }
-  
+
 
   return (
     <div>
