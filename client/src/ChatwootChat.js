c065fbae5189a1b055b2280703c671cdaa95174c
Fırat Can Taş <firatcantass@gmail.com>
2023-07-31 14:59:48 +0300

    🐛 fixed the issue of getting data from the server

--- client/src/ChatwootChat.js
+++ client/src/ChatwootChat.js
@@ -1,144 +1,123 @@
 import React, { useEffect, useState } from 'react'
-import { io } from 'socket.io-client'
 import axios from 'axios'
 
 
-
-
 const ChatwootChat = () => {
+  const [content, setContent] = useState([])
+  const [inputValue, setInputValue] = useState('')
+  const [status, setStatus] = useState('Send Message:')
+  const [socket, setSocket] = useState(null)
+  const inboxIdentifier = 'c1Qm2t1oPjaAAYgzC5vvET7B'
+  const [contactIdentifier, setContactIdentifier] = useState('')
+  const [contactPubsubToken, setContactPubsubToken] = useState('')
+  const [contactConversation, setContactConversation] = useState('')
 
-  const [content, setContent] = useState([]) // Mesaj içeriğini depolamak için kullanılan state
-  const [inputValue, setInputValue] = useState('') // Kullanıcının girdiği metin değeri için kullanılan state
-  const [status, setStatus] = useState('Send Message:') // Sohbet durumunu temsil eden state
-  const [socket, setSocket] = useState(null) // Socket.io bağlantısını temsil eden state
-  const inboxIdentifier = 'c1Qm2t1oPjaAAYgzC5vvET7B' // Chatwoot sunucusundaki gelen kutusu kimliği
-  const [contactIdentifier, setContactIdentifier] = useState('') // Kullanıcının kimliği
-  const [contactPubsubToken, setContactPubsubToken] = useState('') // Kullanıcının pubsub token'ı
-  const [contactConversation, setContactConversation] = useState('') // Kullanıcının sohbet kimliği
+
 
   useEffect(() => {
 
-    const setUpContact = async () => { //Kullanıcı bilgilerini chatwoot sunucusuna gönderilir ve sonucunda response olarak chatwoot kullanıcıya özel bir pubsub token ve source Id oluşturur. 
-
+    const setUpContact = async () => {
       try {
-        const response = await axios.post(`https://app.chatwoot.com/public/api/v1/inboxes/${inboxIdentifier}/contacts`) //Geriye gerekli bilgileri alabilmek için post atıyoruz
 
-        setContactPubsubToken(response.data.pubsub_token) //Pubsub token, publish-subscribe modeline dayalı iletişimi sağlamak için kullanılır. Bu token, kullanıcının belirli bir sohbet kanalına abone olmasını ve o kanal üzerinden mesaj gönderip almasını sağlar.
+        const response = await axios.post(`https://app.chatwoot.com/public/api/v1/inboxes/${inboxIdentifier}/contacts`)
+        console.log("ilk alınan response", response);
+        setContactPubsubToken(response.data.pubsub_token)
+        setContactIdentifier(response.data.source_id)
+        console.log("pubsub token", response.data.pubsub_token)
+        console.log("source id", response.data.source_id)
+        return response
 
-        setContactIdentifier(response.data.source_id) //Kaynak kimliği ise kullanıcının Chatwoot sunucusunda benzersiz bir tanımlayıcıdır. Bu kimlik, kullanıcının belirli bir sohbeti temsil eder ve sunucu üzerindeki işlemleri bu kimlik aracılığıyla yapabiliriz.
-
-        console.log("CONTACT" + response);
-        console.log(response.data.pubsub_token);
-        console.log(response.data.source_id);
-        console.log("-----------------------");
-        console.log(contactIdentifier);
-        console.log(contactPubsubToken);
-
-        return response //setState ler kendi içlerinde asenkron çalıştığı ve buna müdahale edemediğimiz için responsu döndürdüm
       } catch (error) {
         console.log('Contact setup error:', error)
       }
-
     }
 
 
-    const setUpConversation = async (contactResponse) => { //Kullanıcı sohbet kimliğini alıyoruz
 
+    const setUpConversation = async (contactResponse) => {
       try {
-        const response = await axios.post(`https://app.chatwoot.com/public/api/v1/inboxes/${inboxIdentifier}/contacts/${contactResponse.data.source_id}/conversations`) //Geriye gerekli bilgileri alabilmek için post atıyoruz
 
-        setContactConversation(response.data.id) //Sohbet kimliği (contactConversation), Chatwoot platformunda belirli bir müşteri ile yapılan sohbetin benzersiz kimliğidir.
+        const response = await axios.post(`https://app.chatwoot.com/public/api/v1/inboxes/${inboxIdentifier}/contacts/${contactResponse.data.source_id}/conversations`)
+        setContactConversation(response.data.id)
 
-        
+        return contactResponse
       } catch (error) {
         console.log('Conversation setup error:', error)
-        console.log(`CONVERSATION-  https://app.chatwoot.com/public/api/v1/inboxes/${inboxIdentifier}/contacts/${contactIdentifier}/conversations`);
       }
-
     }
 
-    
-
-    const connectToChatwoot = () => {
-      
-
-      const socket = io('ws://localhost:5000/cable') // Socket.io sunucusuna bağlanma
-      setSocket(socket)
 
 
-      socket.on('connect', () => { // Socket.io bağlantısı başarılı olduğunda çalışacak kodlar
 
-        //Bu nesne Chatwoot sunucusunda bir abonelik oluşturmak için gereken parametreleri içerir. Abonelik, belirli bir kanala katılmak ve o kanal üzerinden mesaj alışverişi yapmak için kullanılır.
-        const subscriptionParams = { 
+    const connectToChatwoot = (contactResponse) => {
+      const webSocket = new WebSocket("wss://app.chatwoot.com/cable")
+
+      console.log("socket oluşturuldu", webSocket)
+
+      webSocket.onopen = () => {
+
+      console.log("socket açıldı");
+      console.log("response", contactResponse);
+
+        const subscriptionParams = {
           channel: 'RoomChannel',
-          pubsub_token: contactPubsubToken,
+          pubsub_token: contactResponse.data.pubsub_token,
         }
 
-
-        // Bu nesne WebSocket üzerinden abonelik mesajını temsil eder. command özelliği, abonelik mesajının komutunu belirtir
         const subscription = {
           command: 'subscribe',
           identifier: JSON.stringify(subscriptionParams),
         }
 
+        console.log("sub burda",subscription)
+        webSocket.send(JSON.stringify(subscription))
+        setStatus('Send Message:')
+        console.log('Abone Bilgisi', subscription)
 
-        //Bu satırda socket nesnesi üzerinden subscription nesnesini JSON formatına dönüştürerek abonelik mesajını Chatwoot sunucusuna gönderir. Bu adım, uygulamamızın belirli bir kanala abone olmasını ve o kanal üzerinden mesaj gönderip almasını sağlar.
-        socket.send(JSON.stringify(subscription)) 
+      }
 
-        setStatus('Send Message:') // Durumu güncelleme
-        
-      })
+      webSocket.onmessage = (event) => {
 
+        const serverMessage = JSON.parse(event.data)
+        // console.log('Sunucu mesajı', event)
 
-      socket.on("message", (message) => { //Bu kısım, socket nesnesinin 'message' olayını dinleyen bir olay dinleyicisidir. Yani Chatwoot sunucusundan gelen mesajları dinleyecek olan kod bloğu
-
-        const serverMessage = JSON.parse(message) //Json formatte gelen server mesajımızı kullanabileceğimiz hale çevirdik.
-        console.log(message.data);
-        console.log(...serverMessage);
-
-        // Gelen bu tür mesajlar, WebSocket bağlantısının doğrulanması ve onaylanması ile ilgili bilgilendirmelerdir ve bir işlem yapmamız gerekmez.
         if (serverMessage.type === 'welcome' || serverMessage.type === 'ping' || serverMessage.type === 'confirm_subscription') {
-          console.log("başladı");
           return
-        } 
+        }
+
+        if (serverMessage.message && serverMessage.message.event === 'message.created') {
+            console.log('Sunucu mesajı', event)
+            console.log("mesaj",serverMessage.message)
+            console.log("sunucu mesaj içeriği", serverMessage.message.data.sender)
+            const { name } = serverMessage.message.data.sender
+            const { content } = serverMessage.message.data
+            addMessage(name, content)
+          
 
 
-        //Eğer server dan gelen mesaj türü "message.created" ise mesaj gönderme işlemimiz başarılı demektir. Mesajın içeriği "name" (kimin gönderdiği) ve "content" (mesaj içeriği) i alınarak, addMessage fonksiyonum ile content state i güncellenip arayüzde mesajlar gösterilir.
-        else if (serverMessage.message && serverMessage.message.event === 'message.created') {
-          console.log(`SERVER MSG ALOO 1 : ${serverMessage.message} // ${serverMessage.message.event}`);
-          const { name, content } = serverMessage.message.data.sender
-          addMessage(name, content)
-
-        } 
-
-
+        }
         else {
           console.log('Unknown JSON:', serverMessage)
-          console.log(`SERVER MSG ALOO 2 : ${serverMessage.message} // ${serverMessage.message.event}`);
         }
-        console.log(`SERVER MSG ALOO 3 : ${serverMessage.message} // ${serverMessage.message.event}`);
-      })
 
+      }
 
-      //WebSocket bağlantısı sırasında oluşabilecek hataları dinler ve hata durumunda ilgili bilgiyi konsola yazdırır.
-      socket.on('error', (error) => {
+      webSocket.onerror = (error) => {
         console.log('WebSocket connection error:', error)
+      }
 
-      })
+      setSocket(webSocket)
+      console.log(socket);
     }
 
 
-    //Yazmış olduğumuz fonksiyonları sırayla çağırıp çalıştırdım
+
     setUpContact()
       .then(setUpConversation)
       .then(connectToChatwoot)
-      .catch((error) => {
-        console.log('Setup error:', error)
-      })
-
+      .catch((error) => console.log('Setup error:', error))
 
     return () => {
-
       if (socket) {
         socket.close()
       }
@@ -146,43 +125,53 @@ const ChatwootChat = () => {
 
   }, [])
 
-
-
   const addMessage = (author, message) => {
 
     setContent((prevContent) => [...prevContent, { author, message }])
   }
 
-
   const sendMessage = () => {
-
-    // console.log(`MESSAGE - ${contactIdentifier} // ${contactConversation}`); state içerikleri güncel ulaşıyor
-    
     if (!inputValue) {
       return
     }
 
     const message = { content: inputValue }
+    console.log("contact coversation",contactConversation);
 
     axios.post(`https://app.chatwoot.com/public/api/v1/inboxes/${inboxIdentifier}/contacts/${contactIdentifier}/conversations/${contactConversation}/messages`,message)
-
       .then(() => {
         addMessage('me', inputValue)
         setInputValue('')
       })
-
       .catch((error) => {
         console.log('Message sending error:', error)
       })
-
   }
-
-
 
   const handleInputChange = (event) => {
-
     setInputValue(event.target.value)
   }
+
+
+  // Interval kontrolü
+  // useEffect(() => {
+  //   console.log("Socket burada", socket)
+
+  //   const interval = setInterval(() => {
+  //     if (socket) {
+  //       console.log(socket.readyState)
+
+  //       if (socket.readyState > 1 ) {
+  //         setStatus('Error')
+  //         setInputValue('Unable to communicate with the WebSocket server.')
+  //         setSocket(null)
+  //       }
+  //     }
+  //   }, 3000)
+
+  //   return () => clearInterval(interval)
+  // }, [socket])
+
 
 
   return (
